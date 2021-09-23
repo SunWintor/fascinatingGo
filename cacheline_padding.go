41c4632502887a125f3f85f4ec780e388ef8455c
SunWintor <793815374@qq.com>
2021-09-23 23:34:08 +0800

    采用更加直观的写法

--- cacheline_padding.go
+++ cacheline_padding.go
@@ -18,6 +18,13 @@ type Obj2 struct {
 	num2 uint64
 }
 
+var (
+	runTimes = int64(7000000)
+    wg sync.WaitGroup
+	obj = Obj{}
+	obj2 = Obj2{}
+)
+
 // cpu分为L1 L2 L3三级缓存，其中L1和L2每个核各有一个，而L3则所有核共享。
 // 当L1或者L2更新时，将会触发同步机制MESI（缓存一致性协议） *注意，缓存一致性协议不仅这一个。
 // 同步是有开销的
@@ -32,40 +39,25 @@ func main() {
 }
 
 func testCacheLine() {
-	runTimes := int64(7000000)
-	runTime1 := time.Now().UnixNano()
-	var wg sync.WaitGroup
-	obj := Obj{}
-	obj2 := Obj2{}
+	testRun(func(){atomic.AddUint64(&obj.num, 1)}, func(){atomic.AddUint64(&obj.num2, 1)}, "runTime1")
+	testRun(func(){atomic.AddUint64(&obj2.num, 1)}, func(){atomic.AddUint64(&obj2.num2, 1)}, "runTime2")
+}
+
+func testRun(addFunc, addFunc2 func(), funcName string) {
+	runTime := time.Now().UnixNano()
 	wg.Add(2)
-	go func() { // func1
+	go func() {
 		defer wg.Done()
 		for i := int64(0); i < runTimes; i++ {
-			atomic.AddUint64(&obj.num, 1)
+			addFunc()
 		}
 	}()
-	go func() { // func2
+	go func() {
 		defer wg.Done()
 		for i := int64(0); i < runTimes; i++ {
-			atomic.AddUint64(&obj.num2, 1)
+			addFunc2()
 		}
 	}()
 	wg.Wait()
-	println(fmt.Sprintf("runTime1 = %+v", time.Now().UnixNano()-runTime1))
-	runTime2 := time.Now().UnixNano()
-	wg.Add(2)
-	go func() { // func1
-		defer wg.Done()
-		for i := int64(0); i < runTimes; i++ {
-			atomic.AddUint64(&obj2.num, 1)
-		}
-	}()
-	go func() { // func2
-		defer wg.Done()
-		for i := int64(0); i < runTimes; i++ {
-			atomic.AddUint64(&obj2.num2, 1)
-		}
-	}()
-	wg.Wait()
-	println(fmt.Sprintf("runTime2 = %+v", time.Now().UnixNano()-runTime2))
+	println(fmt.Sprintf("%s = %+v", funcName, time.Now().UnixNano()-runTime))
 }
